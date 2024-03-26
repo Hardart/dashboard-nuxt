960742e780f349647f7b87c4ae2db5ca22f35cd8
Hardart <kaino@bk.ru>
2024-03-27 00:02:48 +0300

    creating program-add-form

--- pages/test.vue
+++ pages/test.vue
@@ -4,23 +4,12 @@ const src = ref('')
 
 <template>
   <UDashboardPage>
-    <UDashboardPanel grow>
-      <div class="absolute inset-0 grid place-items-center">
-        <div class="flex gap-x-4 p-8 bg-slate-100">
-          <div class="relative grid place-items-center size-44 rounded-full bg-teal-500 overflow-clip">
-            <UIcon name="i-heroicons-identification" class="text-4xl" />
-            <input type="file" name="" id="" class="absolute inset-0 opacity-0" />
-          </div>
-          <div class="relative grid place-items-center size-44 rounded-full bg-sky-500 overflow-clip">
-            <UPopover :ui="{ wrapper: 'absolute inset-0' }">
-              <UButton lable="Выбрать" icon="i-heroicons-server" class="absolute inset-0" :ui="{ rounded: 'rounded-full' }" />
-              <template #panel>
-                <UiFileSelect />
-              </template>
-            </UPopover>
-          </div>
-        </div>
-      </div>
+    <UDashboardPanel :resizable="{ min: 200, max: 500 }">
+      <UDashboardPanelContent>
+        <UFormGroup label="Название программы">
+          <UInput />
+        </UFormGroup>
+      </UDashboardPanelContent>
     </UDashboardPanel>
   </UDashboardPage>
 </template>
